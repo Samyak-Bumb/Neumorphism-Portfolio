ed3bcb2256b0b1d87185e6071555e531797e16e6
Samyak-Bumb <vijaybumb0575@gmail.com>
2021-09-24 17:38:44 +0530

    Update style-switcher.js

--- js/style-switcher.js
+++ js/style-switcher.js
@@ -1,12 +1,15 @@
+// ======================== Toggle Style Switcher ========================
 const styleSwitcherToggler = document.querySelector(".style-switcher-toggler");
 styleSwitcherToggler.addEventListener("click",()=>{
     document.querySelector(".style-switcher").classList.toggle("open")
 });
+// Hide styleSwitcherToggler on scroll
 window.addEventListener("scroll",()=>{
     if(document.querySelector(".style-switcher").classList.contains("open")){
         document.querySelector(".style-switcher").classList.remove("open");
     }
 });
+// ======================== Theme Colors ========================
 const alternateStyles = document.querySelectorAll(".alternate-style");
 function setActiveColor(color){
     localStorage.setItem("color",color);
@@ -20,10 +23,12 @@ function changeColor(){
             style.setAttribute("disabled","true");
         }
     })
+    // Checking is "color" key exists
 }
 if(localStorage.getItem("color") !== null){
     changeColor();
 }
+// ======================== Theme Light And Dark ========================
 const nightDay = document.querySelector(".night-day");
 nightDay.addEventListener("click",()=>{
     document.body.classList.toggle("dark");
@@ -56,12 +61,3 @@ function updateIcon(){
         nightDay.querySelector("i").classList.add("bxs-moon");
     }
 }
-// window.onload = ()=>{
-//     if(document.body.classList.contains("dark")){
-//         nightDay.querySelector("i").classList.remove("bxs-moon");
-//         nightDay.querySelector("i").classList.add("bxs-sun");
-//     }else{
-//         nightDay.querySelector("i").classList.remove("bxs-sun");
-//         nightDay.querySelector("i").classList.add("bxs-moon");
-//     }
-// }
