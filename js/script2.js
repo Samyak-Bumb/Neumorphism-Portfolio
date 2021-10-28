b8cb1881a36abbf88610faba10ff53f6801f437a
Samyak Bumb <vijaybumb0575@gmail.com>
2021-10-28 20:51:43 +0530

    Update script2.js

--- js/script2.js
+++ js/script2.js
@@ -1,4 +1,38 @@
-let logo = document.querySelector('logo');
-logo.addEventListener('click', (logo)=>{
-    window.history.back();
+$(document).ready(function() {
+   var progresssPath = document.querySelector('.progressss-wrap path');
+   var pathLength = progresssPath.getTotalLength();
+
+   progresssPath.style.transition = progresssPath.style.WebkitTransition = 'none';
+   progresssPath.style.strokeDasharray = pathLength + ' ' + pathLength;
+   progresssPath.style.strokeDashoffset = pathLength;
+   progresssPath.getBoundingClientRect();
+   progresssPath.style.transition = progresssPath.style.WebkitTransition = 'stroke-dashoffset 10ms linear';
+
+   var updateprogresss = function() {
+      var scroll = $(window).scrollTop();
+      var height = $(document).height() - $(window).height();
+      var progresss = pathLength - (scroll * pathLength / height);
+      progresssPath.style.strokeDashoffset = progresss;
+   }
+
+   updateprogresss();
+   $(window).scroll(updateprogresss);
+
+   var offset = 50;
+   var duration = 550;
+
+   jQuery(window).on('scroll', function() {
+      if(jQuery(this).scrollTop() > offset) {
+         jQuery('.progresss-wrap').addClass('active-progresss');
+      }
+      else {
+         jQuery('.progresss-wrap').removeClass('active-progresss');
+      }
+   });
+
+   jQuery('.progresss-wrap').on('click', function(event) {
+      event.preventDefault();
+      jQuery('html, body').animate({scrollTop: 0}, duration);
+      return false;
+   })
 });
